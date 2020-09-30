00a36a2fec24737d2597f8bb7fb44562706639df
Alexey Cherstvov <krosh.el@gmail.com>
2020-09-30 16:45:55 +0300

    Delete and refresh mode

--- app/src/main/java/ru/netology/lists/ListViewActivity.java
+++ app/src/main/java/ru/netology/lists/ListViewActivity.java
@@ -1,14 +1,18 @@
 package ru.netology.lists;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -18,6 +22,9 @@ import java.util.Map;
 public class ListViewActivity extends AppCompatActivity {
     private static final String KEY_TITLE = "key_title";
     private static final String KEY_COUNT = "key_count";
+    private SharedPreferences savedText;
+    private static String NOTE_TEXT = "saved_text";
+    private static List<Map<String, String>> simpleAdapterContent = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,12 +34,38 @@ public class ListViewActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         ListView list = findViewById(R.id.list);
+        final SwipeRefreshLayout swipeRefresh = findViewById(R.id.swipe_refresh);
+        final BaseAdapter listContentAdapter = createAdapter(simpleAdapterContent);
 
-        List<Map<String, String>> values = prepareContent();
+        String str = getString(R.string.large_text);
+        savedText = getSharedPreferences("SavedText", MODE_PRIVATE);
+        SharedPreferences.Editor editor = savedText.edit();
+        savedText.edit()
+                .putString(NOTE_TEXT, str)
+                .apply();
 
-        BaseAdapter listContentAdapter = createAdapter(values);
+        prepareContent();
 
         list.setAdapter(listContentAdapter);
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                simpleAdapterContent.remove(position);
+                listContentAdapter.notifyDataSetChanged();
+                Toast.makeText(ListViewActivity.this, "Удалено", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        swipeRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                prepareContent();
+                listContentAdapter.notifyDataSetChanged();
+                swipeRefresh.setRefreshing(false);
+                Toast.makeText(ListViewActivity.this, "Обновлено", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @NonNull
@@ -41,16 +74,14 @@ public class ListViewActivity extends AppCompatActivity {
     }
 
     @NonNull
-    private List<Map<String, String>> prepareContent() {
-        List<Map<String, String>> result = new ArrayList<>();
-        String[] titles = getString(R.string.large_text).split("\n\n");
-        for(String title:titles) {
+    private void prepareContent() {
+        String[] titles = savedText.getString(NOTE_TEXT, "").split("\n\n");
+        simpleAdapterContent.clear();
+        for (String title : titles) {
             Map<String, String> map = new HashMap<>();
             map.put(KEY_TITLE, title);
             map.put(KEY_COUNT, String.valueOf(title.length()));
-            result.add(map);
+            simpleAdapterContent.add(map);
         }
-        return result;
     }
-
 }
